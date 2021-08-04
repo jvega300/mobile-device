531ed064ce974f71a80468df8cf0069b7681a8d3
Jorge Vega <jvega300@gmail.com>
2021-08-04 22:12:35 +0200

    feat(add-constants): setup App.jsx
    - develop aApp
    - setup constants

---
+++ src/constants/api.js
@@ -0,0 +1,7 @@
+// Settings
+const API_BASE = "https://front-test-api.herokuapp.com";
+
+
+// API URLS
+export const API_PRODUCT = `${API_BASE}/api/product`;
+export const API_ADD_TO_CART = `${API_BASE}/api/cart`;
